4ca2ad2366faff47a4aea636f5b1694d1ea3f534
boglbbogl <75574246+boglbbogl@users.noreply.github.com>
2024-05-25 19:02:43 +0900

    Initial Commit

---
+++ DDayProject/DDayProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  DDayProjectApp.swift
+//  DDayProject
+//
+//  Created by Tyger Hwang on 5/25/24.
+//
+
+import SwiftUI
+
+@main
+struct DDayProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
